a2dc82a81e979aecef7b72986c37d2ba16691c03
ktmswzw <ktmswzw@gmail.com>
2016-12-07 14:21:28 +0800

    update

---
+++ src/main/java/com/xecoder/common/util/SecurityConstants.java
@@ -0,0 +1,14 @@
+package com.xecoder.common.util;
+
+/**
+ * Created by vincent on 16/9/24.
+ * Duser.name = 224911261@qq.com
+ * Feeling
+ */
+public interface SecurityConstants {
+    public static final String SMS_URL = "http://gw.api.taobao.com/router/rest";
+    public static final String SMSFREESIGNNAME = "SAUGO淑格";
+    public static final String SMS_KEY = "23469164";
+    public static final String SMS_SECRET = "5e4d7a2609b7cc1d22696c49acb30f25";
+    //${name}放了一个宝盒需要您到地亲启：${address}
+}
